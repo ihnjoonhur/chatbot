a953192fe8541b9e0238ca22c1219006915c7b9c
ijhur021 <ihnjoon.h@gmail.com>
2023-09-11 16:31:13 -0400

    update

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--8mopup!bl6b+ylgsh_ch5z))+0fl5q^$gk6op*@&t4n_$d6j6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ijchatbot-be1003375c6b.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
